d19da332ddaae1e0868e039e3d7e08f73fe88f18
Atsuya Uchida <atsuya_eagle@outlook.com>
2021-12-14 05:38:33 +0900

    Fixed url.

--- viewer/src/App.tsx
+++ viewer/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
     setIsLoading(true);
     const width = Number(size.substring(2));
     const height = Number(size.substring(0, 1));
-    axios.get(`/solve/${height}/${width}`).then((res: AxiosResponse<SolverResponse>) => {
+    axios.get(`http://solver:8080/solve/${height}/${width}`).then((res: AxiosResponse<SolverResponse>) => {
       const { data, status } = res;
       console.log(data);
       
