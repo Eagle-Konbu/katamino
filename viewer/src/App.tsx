c70ae55615537efd1f55a35b32b9936de3bf2b48
Atsuya Uchida <atsuya_eagle@outlook.com>
2021-12-14 02:52:37 +0900

    Added spinner.

--- viewer/src/App.tsx
+++ viewer/src/App.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Card, CardBody, Header, Heading, RadioButtonGroup, Text } from 'grommet'
+import { Box, Button, Card, CardBody, Header, Heading, RadioButtonGroup, Spinner, Text } from 'grommet'
 import React from 'react';
 
 import KataminoGrid from './components/KataminoGrid'
@@ -6,12 +6,19 @@ import type { Solution } from './types/Solution'
 
 function App() {
   const [size, setSize] = React.useState('6x10');
+  const [isLoading, setIsLoading] = React.useState(false);
 
   const solution: Solution = {
     width: 10,
     height: 6,
     hexCodes: Array(60).fill("#CC7700")
   };
+  const [solutions, setSolutions] = React.useState<Solution[]>([]);
+
+  const handleClick = () => {
+    setIsLoading(true);
+    setSolutions(Array(4).fill(solution));
+  }
 
   return (
     <div>
@@ -26,15 +33,22 @@ function App() {
             value={size}
             onChange={(e) => { setSize(e.target.value) }}
           />
-          <Button primary label="Solve" style={{ marginTop: '20px' }} />
+          <Button primary label="Solve" onClick={handleClick} style={{ marginTop: '20px' }} />
         </Box>
-        <Text><span>計算時間:5.000s</span><span style={{ marginLeft: '20px' }}>解の個数: 5個</span></Text>
+        <div style={{ display: isLoading ? 'block' : 'none' }}><Spinner /></div>
+        <Text style={{ display: solutions.length != 0 ? 'block' : 'none'}}><span>計算時間:{5}s</span><span style={{ marginLeft: '20px' }}>解の個数: {solutions.length}個</span></Text>
         <Box direction="column" pad="medium">
-          <Card background="light-1">
-            <CardBody pad="medium">
-              <KataminoGrid solution={solution} />
-            </CardBody>
-          </Card>
+          {
+            solutions.map(s => {
+              return (
+                <Card background="light-1" style={{ marginBottom: '30px' }}>
+                  <CardBody pad="medium">
+                    <KataminoGrid solution={s} />
+                  </CardBody>
+                </Card>
+              )
+            })
+          }
         </Box>
       </Box>
     </div>
