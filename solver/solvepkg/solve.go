19174241462ef30e06d33a89d54e70a2209ee60d
Atsuya Uchida <atsuya_eagle@outlook.com>
2022-01-04 17:36:24 +0900

    Fixed complete condition.

--- solver/solvepkg/solve.go
+++ solver/solvepkg/solve.go
@@ -14,15 +14,18 @@ type Piece struct {
 	positions []Point
 }
 
-func Fill(piece Piece, pointIdx int, board [][]string, reset bool) bool {
+var board [][]string
+var solutions [][]string
+
+func Fill(piece Piece, pointIdx int, reset bool) bool {
 	width, height := len(board[0]), len(board)
 	targets := make([]Point, 5)
 	for i, p := range piece.positions {
 		newTarget := Point{x: pointIdx%width + p.x, y: pointIdx/width + p.y}
-		if newTarget.x >= width || newTarget.y >= height {
+		if newTarget.x >= width || newTarget.y >= height || newTarget.x < 0 || newTarget.y < 0 {
 			return false
 		}
-		if board[newTarget.y][newTarget.x] != "" {
+		if board[newTarget.y][newTarget.x] != "" && !reset {
 			return false
 		}
 		targets[i] = newTarget
@@ -38,78 +41,83 @@ func Fill(piece Piece, pointIdx int, board [][]string, reset bool) bool {
 	return true
 }
 
-func Search(pieces []Piece, board [][]string, solutions [][]string) [][]string {
+func Completes() bool {
 	width, height := len(board[0]), len(board)
-	var remainingPieces []Piece
-	clonedBoard := make([][]string, height)
-	for i := range clonedBoard {
-		clonedBoard[i] = make([]string, width)
-	}
-	remainingPieces = append(remainingPieces, pieces...)
-	copy(clonedBoard, board)
-
-	idx := 0
 	for i := 0; i < height; i++ {
 		for j := 0; j < width; j++ {
 			if board[i][j] == "" {
-				idx = i*width + j
-				break
+				return false
 			}
 		}
 	}
+	return true
+}
 
-	for i, piece := range remainingPieces {
-		fmt.Println(clonedBoard)
-		if Fill(piece, idx, clonedBoard, false) {
-			if len(remainingPieces) == 1 {
+func Search(pieces []Piece) {
+	width, height := len(board[0]), len(board)
+
+	idx := 0
+	for i := 0; i < 60; i++ {
+		x, y := i%width, i/width
+		if board[y][x] == "" {
+			idx = i
+			break
+		}
+	}
+
+	for i, piece := range pieces {
+		solution := make([]string, 60)
+		for i := 0; i < height; i++ {
+			for j := 0; j < width; j++ {
+				solution[i*width+j] = board[i][j]
+			}
+		}
+		solutions = append(solutions, solution)
+		if Fill(piece, idx, false) {
+			// fmt.Println("len=", len(pieces), ",i=", i)
+			if Completes() {
+				fmt.Println("found")
 				solution := make([]string, 60)
 				for i := 0; i < height; i++ {
 					for j := 0; j < width; j++ {
-						solution[i*width+j] = clonedBoard[i][j]
+						solution[i*width+j] = board[i][j]
 					}
 				}
 				solutions = append(solutions, solution)
 			} else {
-				Search(append(remainingPieces[:i], remainingPieces[i+1:]...), clonedBoard, solutions)
+				var remainingPieces []Piece
+				remainingPieces = append(remainingPieces, append(pieces[:i], pieces[i+1:]...)...)
+				Search(remainingPieces)
 			}
-			Fill(piece, idx, clonedBoard, true)
+			Fill(piece, idx, true)
 		}
 	}
-	solution := make([]string, 60)
-	for i := 0; i < height; i++ {
-		for j := 0; j < width; j++ {
-			solution[i*width+j] = clonedBoard[i][j]
-		}
-	}
-	solutions = append(solutions, solution)
-	fmt.Println(solutions)
-	return solutions
 }
 
 func Solve(width int, height int) ([][]string, float64) {
 	solutions := make([][]string, 0)
 
-	puzzle := make([][]string, height)
-	for i := range puzzle {
-		puzzle[i] = make([]string, width)
+	board = make([][]string, height)
+	for i := range board {
+		board[i] = make([]string, width)
 	}
 	for i := 0; i < height; i++ {
 		for j := 0; j < width; j++ {
-			puzzle[i][j] = ""
+			board[i][j] = ""
 		}
 	}
 	pieces := []Piece{
-		Piece{color: "#fdf100", positions: []Point{Point{x: 0, y: 0}, Point{x: 0, y: 1}, Point{x: 1, y: 0}, Point{x: 2, y: 1}, Point{x: 2, y: 2}}},
-		Piece{color: "#29005d", positions: []Point{Point{x: 0, y: 0}, Point{x: 0, y: -1}, Point{x: 0, y: -2}, Point{x: 1, y: -2}, Point{x: 1, y: -3}}},
+		Piece{color: "#fdf100", positions: []Point{Point{x: 0, y: 0}, Point{x: 0, y: 1}, Point{x: 1, y: 0}, Point{x: 2, y: 0}, Point{x: 2, y: 1}}},
+		Piece{color: "#29005d", positions: []Point{Point{x: 0, y: 0}, Point{x: 0, y: 1}, Point{x: 0, y: 2}, Point{x: 1, y: 0}, Point{x: 1, y: -1}}},
 		Piece{color: "#66e25a", positions: []Point{Point{x: 0, y: 0}, Point{x: 0, y: 1}, Point{x: 1, y: 0}, Point{x: 1, y: -1}, Point{x: 2, y: -1}}},
 		Piece{color: "#BB0000", positions: []Point{Point{x: 0, y: 0}, Point{x: 1, y: 0}, Point{x: 2, y: 0}, Point{x: 1, y: 1}, Point{x: 1, y: -1}}},
-		Piece{color: "#996e5b", positions: []Point{Point{x: 0, y: 0}, Point{x: 1, y: 0}, Point{x: 2, y: 0}, Point{x: 1, y: 1}, Point{x: 1, y: -1}}},
+		Piece{color: "#996e5b", positions: []Point{Point{x: 0, y: 0}, Point{x: 1, y: 0}, Point{x: 1, y: 1}, Point{x: 1, y: -1}, Point{x: 1, y: -2}}},
 		Piece{color: "#234c83", positions: []Point{Point{x: 0, y: 0}, Point{x: 1, y: 0}, Point{x: 1, y: -1}, Point{x: 1, y: -2}, Point{x: 2, y: -2}}},
 		Piece{color: "#808080", positions: []Point{Point{x: 0, y: 0}, Point{x: 1, y: 0}, Point{x: 1, y: -1}, Point{x: 1, y: -2}, Point{x: 2, y: -1}}},
-		Piece{color: "#000080", positions: []Point{Point{x: 0, y: 0}, Point{x: 0, y: -1}, Point{x: 0, y: -2}, Point{x: 0, y: -3}, Point{x: 0, y: -4}}},
-		Piece{color: "#dad400", positions: []Point{Point{x: 0, y: 0}, Point{x: 0, y: -1}, Point{x: 0, y: -2}, Point{x: 0, y: -3}, Point{x: 1, y: -3}}},
-		Piece{color: "#62b7ff", positions: []Point{Point{x: 0, y: 0}, Point{x: 0, y: -1}, Point{x: 0, y: -2}, Point{x: 1, y: -2}, Point{x: 2, y: -2}}},
-		Piece{color: "#ffc0cb", positions: []Point{Point{x: 0, y: 0}, Point{x: 0, y: -1}, Point{x: 0, y: -2}, Point{x: 1, y: 0}, Point{x: 1, y: -1}}},
+		Piece{color: "#000080", positions: []Point{Point{x: 0, y: 0}, Point{x: 0, y: 1}, Point{x: 0, y: 2}, Point{x: 0, y: 3}, Point{x: 0, y: 4}}},
+		Piece{color: "#dad400", positions: []Point{Point{x: 0, y: 0}, Point{x: 0, y: 1}, Point{x: 0, y: 2}, Point{x: 0, y: 3}, Point{x: 1, y: 0}}},
+		Piece{color: "#62b7ff", positions: []Point{Point{x: 0, y: 0}, Point{x: 0, y: 1}, Point{x: 0, y: 2}, Point{x: 1, y: 0}, Point{x: 2, y: 0}}},
+		Piece{color: "#ffc0cb", positions: []Point{Point{x: 0, y: 0}, Point{x: 0, y: 1}, Point{x: 0, y: 2}, Point{x: 1, y: 1}, Point{x: 1, y: 2}}},
 		Piece{color: "#004900", positions: []Point{Point{x: 0, y: 0}, Point{x: 1, y: 0}, Point{x: 2, y: 0}, Point{x: 1, y: -1}, Point{x: 1, y: -2}}},
 	}
 
@@ -163,7 +171,7 @@ func Solve(width int, height int) ([][]string, float64) {
 
 	startTime := time.Now()
 
-	solutions = Search(pieces, puzzle, solutions)
+	Search(pieces)
 
 	for i := range solutions {
 		for j := range solutions[i] {
@@ -172,19 +180,6 @@ func Solve(width int, height int) ([][]string, float64) {
 			}
 		}
 	}
-
-	// for i := 0; i < 30; i++ {
-	// 	solTmp = append(solTmp, "#AA0000")
-	// }
-	// for i := 0; i < 30; i++ {
-	// 	solTmp = append(solTmp, "#00AA00")
-	// }
-
-	// for i := 0; i < 4; i++ {
-	// 	tmp := make([]string, len(solTmp))
-	// 	copy(tmp, solTmp)
-	// 	solutions[i] = tmp
-	// }
 
 	calcTime := float64(time.Since(startTime).Milliseconds()) / 1000
 
